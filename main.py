a3097f7d59934caf5ba1c6b714c2a1b841f76832
musi17777 <netanel17777@gmail.com>
2024-09-22 21:12:43 +0300

    finish till part 2:

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+# Part 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# student 1: Netanel Musayev (5535)
 
+# Part 2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Loading datasets
+train_data = pd.read_csv('datasets/titanic_train.csv')
+test_data = pd.read_csv('datasets/titanic_test.csv')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Test loading
+print("Train Data:")
+print(train_data.head())
+print("\nTest Data:")
+print(test_data.head())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Statistics
+
+# Basic
+print("Basic statistics:")
+print(train_data.describe())
+
+# Number of passengers who survived
+survival_count = train_data['Survived'].value_counts()
+print("Passengers who survived:")
+print(survival_count)
+
+# Chart describes distribution of ages
+plt.figure(figsize=(10, 6))
+sns.histplot(train_data['Age'].dropna(), bins=30, kde=True)
+plt.title('Distribution of ages')
+plt.xlabel('age')
+plt.ylabel('number of passengers')
+plt.savefig('statistics/age_distribution.png')
+plt.show()
+
+# Preprocessing
+train_data['Survived'] = train_data['Survived'].map({0: 'not survived', 1: 'survived'})
+
+# Chart describes survival cout by gender
+plt.figure(figsize=(8, 6))
+sns.countplot(x='Sex', hue='Survived', data=train_data)
+plt.title('survival count by gender')
+plt.xlabel('gender')
+plt.ylabel('number of passengers')
+plt.legend(title='Survived', loc='upper right', labels=['not survived', 'survived'])
+plt.savefig('statistics/survivals_by_gender.png')
+plt.show()
+
+# Part 3
+
